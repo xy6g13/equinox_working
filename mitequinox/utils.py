9aea4f448cbfcdfbdda303eb267e0904682d5c8d
Xiaolong Yu <xy6g13@gmail.com>
2018-09-27 15:14:48 +0000

    preliminary results

--- mitequinox/utils.py
+++ mitequinox/utils.py
@@ -218,3 +218,25 @@ def plot_scalar(v, colorbar=False, title=None, vmin=None, vmax=None, savefig=Non
         #
         if not offline:
             plt.show()
+
+#           
+def quick_llc_plot(data, axis_off=False, **kwargs):
+    face_to_axis = {0: (2, 0), 1: (1, 0), 2: (0, 0),
+                    3: (2, 1), 4: (1, 1), 5: (0, 1),
+                    7: (0, 2), 8: (1, 2), 9: (2, 2),
+                    10: (0, 3), 11: (1, 3), 12: (2, 3)}
+    transpose = [7, 8, 9, 10, 11, 12]
+    gridspec_kw = dict(left=0, bottom=0, right=4, top=4, wspace=0, hspace=0)
+    fig, axes = plt.subplots(nrows=3, ncols=4, gridspec_kw=gridspec_kw)
+    for face, (j, i) in face_to_axis.items():
+        data_ax = data.isel(face=face)
+        ax = axes[j,i]
+        yincrease = True
+        if face in transpose:
+            data_ax = data_ax.transpose()
+            yincrease = False
+        data_ax.plot(ax=ax, yincrease=yincrease, **kwargs)
+        if axis_off:
+            ax.axis('off')
+        ax.set_title('')
+    
