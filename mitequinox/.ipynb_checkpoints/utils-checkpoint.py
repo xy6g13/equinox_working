1a1dd0c8d6dc14162b53aa1382f3cf74cd36de74
Xiaolong Yu <xy6g13@gmail.com>
2018-10-03 12:29:20 +0000

    updated

--- mitequinox/.ipynb_checkpoints/utils-checkpoint.py
+++ mitequinox/.ipynb_checkpoints/utils-checkpoint.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+from scipy.signal import welch
 import dask
 import xarray as xr
 import threading
@@ -8,18 +9,26 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 from cmocean import cm
 
+import datetime, dateutil
+
 import xmitgcm as xm
 
+#------------------------------ paths ---------------------------------------
 
 #tmp = os.getenv('TMPDIR')
-datawork = os.getenv('DATAWORK')
-home = os.getenv('HOME')
-scratch = os.getenv('SCRATCH')
+datawork = os.getenv('DATAWORK')+'/'
+home = os.getenv('HOME')+'/'
+scratch = os.getenv('SCRATCH')+'/'
 osi = '/home/datawork-lops-osi/aponte/'
 #
 root_data_dir = '/home/datawork-lops-osi/data/mit4320/'
 grid_dir = root_data_dir+'grid/'
 
+#llc4320 = xm.utils.get_extra_metadata(domain='llc', nx=4320)
+
+#------------------------------ mit specific ---------------------------------------
+
+#
 def get_compressed_level_index(grid_dir, index_fname='llc4320_compressed_level_index.nc', geometry='llc'):
     ''' Some doc
     '''
@@ -27,7 +36,7 @@ def get_compressed_level_index(grid_dir, index_fname='llc4320_compressed_level_i
     ds = xm.open_mdsdataset('', grid_dir=grid_dir,
                              iters=None, geometry=geometry, read_grid=True,
                              default_dtype=np.dtype('>f4'),
-                             ignore_unknown_vars=True)
+                             ignore_unknown_vars=True) #, extra_metadata=llc4320)
     
     # get shape
     #nz, nface, ny, nx = ds.hFacC.shape
@@ -118,7 +127,11 @@ def get_compressed_data(varname, data_dir, grid_dir, ds_index=None, ds=None, ite
     
     # load mask from raw data
     hfac = xm.utils.read_mds(grid_dir + 'hFac' + point,
-                            use_mmap=True, dask_delayed=False, force_dict=False)
+                             use_mmap=True, dask_delayed=False, force_dict=False) 
+    #hfac = xm.utils.read_mds(grid_dir + 'hFac' + point, llc=True,
+    #                         use_mmap=True, use_dask=False, extra_metadata=llc4320)['hFac' + point]
+    #                        use_mmap=True, use_dask=False, force_dict=False)['hFac' + point]
+    #hfac = xm.utils.read_3d_llc_data(grid_dir + 'hFac'+point+'.data', 90, 4320, dtype='>f4', memmap=True)
     mask = hfac[k]>0
     if client is None:
         mask_future = mask
@@ -128,19 +141,22 @@ def get_compressed_data(varname, data_dir, grid_dir, ds_index=None, ds=None, ite
     data = dask.array.concatenate([lazily_load_level_from_3D_field
                             (data_dir, varname, i, offset, count, mask_future, shape, dtype)
                             for i in iters], axis=0)
-    
+
     if point is 'C':
         dims = ['time', 'face', 'j', 'i']
     elif point is 'W':
         dims = ['time', 'face', 'j', 'i_g']
     elif point is 'S':
-        dims = ['time', 'face', 'j_g', 'i']
+        dims = ['time', 'face', 'j_g', 'i']    
     
-    ds[varname] = xr.Variable(dims, data)
+    ds[varname] = xr.Variable(dims, data)   
     
     if time is not None:
         ds['time'] = time.sel(iters=iters).values
-    
+        #ds['dtime'] = iters_to_date(iters)
+        ds = ds.assign_coords(dtime=xr.DataArray(iters_to_date(iters), dims=['time']))
+        #ds = ds.assign(dtime=)
+        
     return ds
 
 
@@ -175,13 +191,18 @@ def get_iters_time(varname, data_dir, delta_t=25.):
     
     return iters, time
 
+def iters_to_date(iters, delta_t=25.):
+    t0 = datetime.datetime(2011,9,13)    
+    ltime = delta_t * (np.array(iters)-10368)
+    dtime = [t0+dateutil.relativedelta.relativedelta(seconds=t) for t in ltime]    
+    return dtime
 
-    
+
 #------------------------------ plot ---------------------------------------
 
 #
 def plot_scalar(v, colorbar=False, title=None, vmin=None, vmax=None, savefig=None, 
-                offline=False, coast_resolution='110m', figsize=(10,10), cmmap='balance'):
+                offline=False, coast_resolution='110m', figsize=(10,10), cmmap='thermal'):
     #
     if vmin is None:
         vmin = v.min()
@@ -219,17 +240,17 @@ def plot_scalar(v, colorbar=False, title=None, vmin=None, vmax=None, savefig=Non
         if not offline:
             plt.show()
 
-#           
+#
 def quick_llc_plot(data, axis_off=False, **kwargs):
     face_to_axis = {0: (2, 0), 1: (1, 0), 2: (0, 0),
                     3: (2, 1), 4: (1, 1), 5: (0, 1),
                     7: (0, 2), 8: (1, 2), 9: (2, 2),
                     10: (0, 3), 11: (1, 3), 12: (2, 3)}
     transpose = [7, 8, 9, 10, 11, 12]
-    gridspec_kw = dict(left=0, bottom=0, right=4, top=4, wspace=0, hspace=0)
+    gridspec_kw = dict(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)
     fig, axes = plt.subplots(nrows=3, ncols=4, gridspec_kw=gridspec_kw)
     for face, (j, i) in face_to_axis.items():
-        data_ax = data.isel(face=face)
+        data_ax = data.sel(face=face)
         ax = axes[j,i]
         yincrease = True
         if face in transpose:
@@ -239,4 +260,183 @@ def quick_llc_plot(data, axis_off=False, **kwargs):
         if axis_off:
             ax.axis('off')
         ax.set_title('')
+            
+            
+#------------------------------ zarr ---------------------------------
+
+def store_zarr(ds, filename, encoding):
+    # tmp, waiting for xarray release
+    for v in ds.variables:
+        if hasattr(ds[v],'encoding') and 'chunks' in ds[v].encoding:
+            del ds[v].encoding['chunks']    
+    #
+    ds.to_zarr(filename, mode='w', encoding=encoding)
+
+def zarr_standard(V, client, F=None, out_dir=None, compressor=None):
+    """ rechunk variables
+    
+    Parameters
+    ----------
+    V: list of str
+        variables to rechunk
+
+    face: list of int
+        faces to consider, default is all (0-12)
+        
+    out_dir: str
+        output directory
+        
+    """
+
+    if out_dir is None:
+        out_dir = scratch+'mit/standard/'
+
+    if F is None:
+        F = range(13)
+    elif type(F) is int:
+        F = [F]
+        
+    for v in V:
+        #
+        data_dir = root_data_dir+v+'/'
+        iters, time = get_iters_time(v, data_dir, delta_t=25.)
+        #
+        p = 'C'
+        if v is 'SSU':
+            p = 'W'
+        elif v is 'SSV':
+            p = 'S'
+        #
+        ds = get_compressed_data(v, data_dir, grid_dir, iters=iters, 
+                                 time=time, client=client, point=p)
+        #
+        # should store grid data independantly in a single file
+        ds = ds.drop(['XC','YC','Depth','rA'])
+        #
+        ds = ds.sel(face=F)
+        ds = ds.chunk({'face': 1})
+        #
+        dv = ds[v].to_dataset()
+        #
+        file_out = out_dir+'/%s.zarr'%(v)
+        try:
+            dv.to_zarr(file_out, mode='w', \
+                       encoding={key: {'compressor': compressor} for key in dv.variables})
+        except:
+            print('Failure')
+        dsize = getsize(file_out)
+        print(' %s converted to zarr,  data is %.1fGB ' %(v, dsize/1e9))
+
+        
+#------------------------------ rechunking ---------------------------------
+
+def rechunk(V, F=None, out_dir=None, Nt = 24*10, Nc = 96, compressor=None):
+    """ rechunk variables
+    
+    Parameters
+    ----------
+    V: list of str
+        variables to rechunk
+
+    F: list of int
+        faces to consider, default is all (0-12)
+        
+    out_dir: str
+        output directory
+        
+    Nt: int
+        temporal chunk size, default is 24x10
+    
+    Nc; int, tuple
+        i, j chunk size, default is (96, 96) (96x45=4320)
+        
+    """
+
+    if out_dir is None:
+        out_dir = scratch+'mit/rechunked/'
+
+    if F is None:
+        F=range(13)
+    elif type(F) is int:
+        F = [F]
+        
+    if type(Nc) is int:
+        Ni, Nj = Nc
+    elif type(Nc) is tuple:
+        Ni, Nj = Nc[0], Nc[1]
+        
+    for v in V:
+
+        file_in = scratch+'/mit/standard/%s.zarr'%(v)
+        ds0 = xr.open_zarr(file_in)
+                
+        for face in F:
+
+            ds = ds0.sel(face=face)
+            #
+            ds = ds.isel(time=slice(len(ds.time)//Nt *Nt))
+            #
+            ds = ds.chunk({'time': Nt, 'i': Ni, 'j': Nj})
+            #
+            # tmp, xarray zarr backend bug: 
+            # https://github.com/pydata/xarray/issues/2278
+            del ds['face'].encoding['chunks']
+            del ds[v].encoding['chunks']
+
+            file_out = out_dir+'%s_f%02d.zarr'%(v,face)
+            try:
+                if compressor is 'default':
+                    ds.to_zarr(file_out, mode='w')
+                else:
+                    # specify compression:
+                    ds.to_zarr(file_out, mode='w', \
+                                encoding={key: {'compressor': compressor} for key in ds.variables})
+            except:
+                print('Failure')
+            dsize = getsize(file_out)
+            print(' %s face=%d  data is %.1fGB ' %(v, face, dsize/1e9))
+
+
+#------------------------------ spectrum ---------------------------------------
+            
+def _get_E(x, ufunc=True, **kwargs):
+    ax = -1 if ufunc else 0
+    #
+    dkwargs = {'window': 'hann', 'return_onesided': False, 
+               'detrend': 'linear', 'scaling': 'density'}
+    dkwargs.update(kwargs)
+    f, E = welch(x, fs=24., axis=ax, **dkwargs)
+    #
+    if ufunc:
+        return E
+    else:
+        return f, E
+
+def get_E(v, f=None, **kwargs):
+    v = v.chunk({'time': len(v.time)})
+    if 'nperseg' in kwargs:
+        Nb = kwargs['nperseg']
+    else:
+        Nb = 80*24
+        kwargs['nperseg']= Nb
+    if f is None:
+        f, E = _get_E(v.values, ufunc=False, **kwargs)
+        return f, E
+    else:
+        E = xr.apply_ufunc(_get_E, v,
+                    dask='parallelized', output_dtypes=[np.float64],
+                    input_core_dims=[['time']],
+                    output_core_dims=[['freq_time']],
+                    output_sizes={'freq_time': Nb}, kwargs=kwargs)
+        return E.assign_coords(freq_time=f).sortby('freq_time')
+
+#------------------------------ misc ---------------------------------------
+                        
+def getsize(dir_path):
+    ''' Returns the size of a directory in bytes
+    '''
+    process = os.popen('du -s '+dir_path)
+    size = int(process.read().split()[0]) # du returns kb
+    process.close()
+    return size*1e3            
     
